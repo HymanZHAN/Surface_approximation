7e51bd29fe82c9875c19ae533644a0f98989ce20
Xucong ZHAN <zhanxucong@gmail.com>
2017-05-04 13:29:42 +0800

    add #include <pcl/filters/project_inliers.h>

--- source/patch_recognition.h
+++ source/patch_recognition.h
@@ -28,6 +28,7 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/project_inliers.h>
 
 #include <boost/config.hpp>
 #include <boost/graph/adjacency_list.hpp>
